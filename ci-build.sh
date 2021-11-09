0c006ddb5c4ba45f318fd380521958e82f8c9fef
Alexander Barth <barth.alexander@gmail.com>
2021-11-09 14:50:34 +0100

    Update ci-build.sh

--- ci-build.sh
+++ ci-build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+set -e
 
 upstream_version=4.8.1
 wget https://github.com/Unidata/netcdf-c/archive/v${upstream_version}.tar.gz
 tar -xf v${upstream_version}.tar.gz
 cd netcdf-*
-./configure --prefix=/builddir
-make -j4
+./configure --prefix=/builddir     -enable-shared  --disable-static
+make -j4 LDFLAGS="-no-undefined  -Wl,--export-all-symbols"
 make check
 make install
